de441ec2bcd6f5a623d8ba0e793ee358c69a30e3
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-25 13:53:12 -0500

    Get All Folders (no searchTerm needed)

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -6,5 +6,16 @@ const router = express.Router();
 
 const knex = require('../knex');
 
+router.get('/folders', (req, res, next) => {
+  knex
+    .select('id', 'name')
+    .from('folders')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
