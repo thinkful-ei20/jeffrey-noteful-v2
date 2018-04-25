6630f55394f5f51d97f67abffff5333b1f0bf528
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-25 15:24:39 -0500

    Create a new Note

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -21,7 +21,7 @@ router.get('/notes', (req, res, next) => {
       }
     })
     .modify(function (queryBuilder) {
-      if(folderId) {
+      if (folderId) {
         queryBuilder.where('folder_id', folderId);
       }
     })
@@ -86,9 +86,9 @@ router.put('/notes/:id', (req, res, next) => {
 
 // Post (insert) an item
 router.post('/notes', (req, res, next) => {
-  const { title, content } = req.body;
+  const { title, content, folder_id } = req.body;
 
-  const newItem = { title, content };
+  const newItem = { title, content, folder_id };
   /***** Never trust users - validate input *****/
   if (!newItem.title) {
     const err = new Error('Missing `title` in request body');
@@ -96,11 +96,20 @@ router.post('/notes', (req, res, next) => {
     return next(err);
   }
 
+  let noteId;
+
   knex.insert(newItem)
     .into('notes')
-    .returning(['id', 'title', 'content'])
-    .then((results) => {
-      res.location(`${req.originalUrl}/${results[0].id}`).status(201).json(results[0]);
+    .returning('id')
+    .then(([id]) => {
+      noteId = id;
+      return knex.select('notes.id', 'title', 'content', 'folder_id', 'folders.name as folder_name')
+        .from('notes')
+        .leftJoin('folders', 'notes.folder_id', 'folders.id')
+        .where('notes.id', noteId)
+    })
+    .then(([result]) => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
     })
     .catch(err => next(err));
 });
