e7030763055051dd35ce2f709aa540a7f2f97759
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-26 11:08:45 -0500

    GET route

--- routes/tags.router.js
+++ routes/tags.router.js
@@ -6,6 +6,18 @@ const router = express.Router();
 
 const knex = require('../knex');
 
+router.get('/tags', (req, res, next) => {
+  knex
+    .select('id', 'name')
+    .from('tags')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 router.post('/tags', (req, res, next) => {
   const { name } = req.body;
 
