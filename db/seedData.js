746d1cee8d12f76327d759fce109215a6a1d943a
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-27 14:38:09 -0500

    Configure NPM script to run tests

---
+++ db/seedData.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const knex = require('../knex');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = function (file, user = 'dev') {
+  //return exec(`psql -f ${file} -d ${knex.client.connectionSettings.database}`);
+  return exec(`psql -f ${file} -d postgres://dev:dev@localhost/noteful-test`);
+};
