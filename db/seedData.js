c68d97bcba9a11c73d0264fedf4ed855b0e20a31
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-27 15:53:36 -0500

    Test Travis Password

--- db/seedData.js
+++ db/seedData.js
@@ -6,5 +6,5 @@ const exec = util.promisify(require('child_process').exec);
 
 module.exports = function (file, user = 'dev') {
   //return exec(`psql -f ${file} -d ${knex.client.connectionSettings.database}`);
-  return exec(`psql -f ${file} -d postgres://postgres:@localhost/noteful-test`);
+  return exec(`psql -f ${file} -d postgres://postgres@localhost/noteful-test`);
 };
